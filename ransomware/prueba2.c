b5382535769a4382fdda276c457913094525e164
nsh255 <nsh255@inlumine.ual.es>
2023-10-19 18:56:02 +0200

    cosas varias

--- ransomware/prueba2.c
+++ ransomware/prueba2.c
@@ -1,51 +1,25 @@
-#include <stdio.h>
 #include <windows.h>
-#include <shlwapi.h>
-
-int CopyFiles(const char *sourcePath, const char *destPath) {
-    WIN32_FIND_DATA findFileData;
-    HANDLE hFind = FindFirstFile(strcat(strcat(sourcePath, "\\"), "*"), &findFileData);
-
-    if (hFind == INVALID_HANDLE_VALUE) {
-        return 1; // Error al buscar archivos en la carpeta de origen.
-    }
-
-    do {
-        if (!(findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-            char sourceFilePath[MAX_PATH];
-            char destFilePath[MAX_PATH];
-
-            PathCombineA(sourceFilePath, sourcePath, findFileData.cFileName);
-            PathCombineA(destFilePath, destPath, findFileData.cFileName);
-
-            if (!CopyFile(sourceFilePath, destFilePath, FALSE)) {
-                return 2; // Error al copiar archivos.
-            }
-        }
-    } while (FindNextFile(hFind, &findFileData) != 0);
-
-    FindClose(hFind);
-    return 0; // Éxito.
-}
+#include <tchar.h>
+#include <stdio.h>
 
 int main() {
-    const char *sourceFolder = "C:\\Users\\usuario\\Desktop\\Pruebas para criptografía";
-    const char *destFolder = "C:\\Users\\usuario\\Desktop\\Archivos encriptados";
-
-    if (!CreateDirectory(destFolder, NULL) && ERROR_ALREADY_EXISTS != GetLastError()) {
-        printf("Error al crear la carpeta de destino. Código de error: %d\n", GetLastError());
+    WIN32_FIND_DATA findFileData;
+    HANDLE hFind = FindFirstFile(_T("C:\\Users\\usuario\\Desktop\\patata"), &findFileData);
+    
+    if (hFind == INVALID_HANDLE_VALUE) {
+        _tprintf(_T("No se pudo abrir la carpeta. Error %d: %s\n"), GetLastError(), strerror(GetLastError()));
         return 1;
+    } else {
+        int numero;
+        do {
+            if (!(findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+                _tprintf(_T("Nombre del archivo: %s\n"), findFileData.cFileName);
+                _tprintf(_T("Ingresa un número: "));
+                scanf("%d", &numero);
+            }
+        } while (FindNextFile(hFind, &findFileData) != 0);
+        FindClose(hFind);
     }
 
-    int result = CopyFiles(sourceFolder, destFolder);
-
-    if (result == 0) {
-        printf("Archivos copiados con éxito.\n");
-    } else if (result == 1) {
-        printf("Error al buscar archivos en la carpeta de origen.\n");
-    } else if (result == 2) {
-        printf("Error al copiar archivos.\n");
-    }
-
-    return result;
+    return 0;
 }
--- ransomware/prueba3.c
+++ ransomware/prueba3.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <windows.h>
+#include <tchar.h>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 
